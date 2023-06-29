895e1e1f014d27c8b460f94b51226ba9d5867542
FreedomSakura <66147729+FreedomSakura@users.noreply.github.com>
2023-06-29 11:46:44 +0800

    Sample 100

--- RayTracing In One Weekend/main.cc
+++ RayTracing In One Weekend/main.cc
@@ -101,7 +101,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 10;
+    const int samples_per_pixel = 100;
     const int max_depth = 50;
 
     // World
@@ -131,21 +131,6 @@ int main() {
 
 #pragma omp parallel private(tid)
     {
-		//for (int j = image_height - 1; j >= 0; --j) {
-		//	std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-		//	for (int i = 0; i < image_width; ++i) {
-		//		color pixel_color(0, 0, 0);
-		//		for (int s = 0; s < samples_per_pixel; ++s) {
-		//			auto u = (i + random_double()) / (image_width - 1);
-		//			auto v = (j + random_double()) / (image_height - 1);
-		//			ray r = cam.get_ray(u, v);
-		//			pixel_color += ray_color(r, world, max_depth);
-		//		}
-		//		write_color(std::cout, pixel_color, samples_per_pixel);
-		//	}
-		//}
-
-
 		for (int j = 0; j < image_height; j++) {
 			std::cerr << "\rScanlines remaining: " << image_height - j << ' ' << std::flush;
 			for (int i = 0; i < image_width; ++i) {
